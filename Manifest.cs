7e5f222f3eece5dfe6b065b28cd302bc8c28e0a9
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-10-21 01:47:14 +0200

    Updating manifest

--- Manifest.cs
+++ Manifest.cs
@@ -3,8 +3,8 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "CSM.Bynder",
     Author = "City of Santa Monica",
-    Website = "https://santamonica.gov",
-    Version = "0.0.1",
+    Website = "https://github.com/CityofSantaMonica/Orchard-Core-Bynder",
+    Version = "1.0.0",
     Description = "Integration with the digital asset management platform Bynder.",
     Category = "CSM"
 )]
